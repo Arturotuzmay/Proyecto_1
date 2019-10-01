ab1a9fce5337ce92c5a16eadf89f0eb2757d171b
unknown <manuel314159265@gmail.com>
2019-10-01 10:00:25 -0500

    commit prueba

--- regex.js
+++ regex.js
@@ -1,4 +1,4 @@
 //Ejemplo que borra comentarios
 let mensaje = "/*Este es un comentario*/ Esto no es un comentario /*Comentario*/ :v";
-let newMensaje = mensaje.replace(/[\/][*][\w ]*[*][\/]/g, ""); //Reemplaza los comentarios por vacío
-console.log(newMensaje);
+let newMensaje = mensaje.replace(/[\/][*][\w ]*[*][\/]/g, ""); //Reemplaza Los comentarios por vacío
+// console.log(newMensaje);
