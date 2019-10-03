df08410541c039a6d417d0e1f8f2b9c52fb6fc54
unknown <manuel314159265@gmail.com>
2019-10-03 01:49:27 -0500

    Nuevo apartado

--- regex.js
+++ regex.js
@@ -1,27 +1,3 @@
-let stringText;
-function leerArchivo(e) {
-  var archivo = e.target.files[0];
-  if (!archivo) {
-    return;
-  }
-  var lector = new FileReader();
-  lector.onload = function(e) {
-    var contenido = e.target.result;
-    mostrarContenido(contenido);
-    stringText = contenido;
-  };
-  lector.readAsText(archivo);
-}
-
-function mostrarContenido(contenido) {
-  var elemento = document.getElementById('contenido-archivo');
-  elemento.innerHTML = contenido;
-}
-
-document.getElementById('file-input').addEventListener('change', leerArchivo, false);
-
-
-
 var fs = require('fs'),
     readline = require('readline');
 
