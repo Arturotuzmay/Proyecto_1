26bd1bf6a2b07d755065a70bec495e13305c56a9
Carlos Daniel Ciau Nahuat <carlosciau10@gmail.com>
2019-10-04 02:07:20 -0500

    Modificacion de detalles

--- Programa_Ejemplo.java
+++ Programa_Ejemplo.java
@@ -1,13 +1,10 @@
 public class Programa_Ejemplo {
 
-   
-    /*este
-    es un comentario
-    multilinea */
     public static void main (String[] args) {
-    	
     //Este es un programa de ejemplo
     //Lenguajes y Automatas	
+
+    /*Declaracion de Variables*/
  	int res1, res2, res3, res4;
     int x = 10;
     int y = 20;
@@ -17,19 +14,19 @@ public class Programa_Ejemplo {
     if (y<x){
     	System.out.println("Resultado 1");
     	}else
-     if (y>x){
-    	System.out.println("Resultado 2");
+    if (y>x){
+    	System.out.println("Resultado 2"); //Resultado 2
     	}else
-     if (y<=x){
+    if (y<=x){
     	System.out.println("Resultado 3");
     	}else
-     if (y>=x){
-    	System.out.println("Resultado 4");
+    if (y>=x){
+    	System.out.println("Resultado 4");/*Resultado 4*/
     	}else
  	 if (y!=x){
     	System.out.println("Resultado 5");
     	}else
-     if (y==x){
+    if (y==x){
     	System.out.println("Resultado 6");
     	}
     res1 = x + y;
